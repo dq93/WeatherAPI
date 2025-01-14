a5496f6e5e871d0376739831cca8b82835271261
Daniel <danielquintanilladq93@gmail.com>
2025-01-13 22:58:23 -0500

    uploading json and filtered csv files

--- weather_api.py
+++ weather_api.py
@@ -4,14 +4,36 @@ from bs4 import BeautifulSoup
 import json
 import csv
 
-URL = 'https://historical-forecast-api.open-meteo.com/v1/forecast?latitude=-21.72&longitude=-45.39&start_date=2022-01-01&end_date=2023-12-31&hourly=temperature_2m,relative_humidity_2m,precipitation,surface_pressure'
+URL = "https://historical-forecast-api.open-meteo.com/v1/forecast?latitude=-21.72&longitude=-45.39&start_date=2022-01-01&end_date=2023-12-31&hourly=temperature_2m,relative_humidity_2m,precipitation,surface_pressure"
 
 response = requests.get(URL)
-data = response.json()
 
-output = 'data/json/weather_data.json'
+if response.status_code == 200:
+    
+    data = response.json()
 
-with open(output, 'w') as f:
-    json.dump(data, f, indent=4)
+    json_data_filepath = "data/json/weather_data.json"
 
-#CSV_PATH = 'data/CSV/weather_data.csv'
+    with open(json_data_filepath, "w") as file:
+        json.dump(data, file, indent = 4)
+    
+    df = pd.DataFrame(data)
+
+    hourly_data = data["hourly"]
+
+    cleaned_data = {
+        "time": hourly_data["time"],
+        "temperature_2m": hourly_data["temperature_2m"],
+        "relative_humidity_2m": hourly_data["relative_humidity_2m"],
+        "precipitation": hourly_data["precipitation"],
+        "surface_pressure": hourly_data["surface_pressure"]
+    }
+
+    new_df = pd.DataFrame(cleaned_data)
+
+    csv_filepath = "data/csv/weather_data.csv"
+
+    new_df.to_csv(csv_filepath, index=False)
+    
+else:
+    print("error unable to access")
